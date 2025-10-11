5c08b0ebb68b7dabf5d7aad49b935976f1bca1b0
Thibaut CHAUFFIER <tchauffi@gmail.com>
2025-10-11 13:43:01 +0200

    ADD: player embedding to let the model know who have to play

--- src/chesstransformer/models/transformer/position2move.py
+++ src/chesstransformer/models/transformer/position2move.py
@@ -72,6 +72,7 @@ class Position2MoveModel(nn.Module):
         super().__init__()
         self.token_embedding = nn.Embedding(vocab_size, embed_dim)
         self.position_embedding = nn.Embedding(64, embed_dim)  # 64 squares
+        self.player_embedding = nn.Embedding(2, embed_dim)  # 2 players: white and black
         self.dropout = nn.Dropout(dropout)
         self.transformer_blocks = nn.Sequential(
             *[TransformerBlock(
@@ -87,7 +88,7 @@ class Position2MoveModel(nn.Module):
 
         self.lm_head = nn.Linear(embed_dim, move_vocab_size, bias=False)
 
-    def forward(self, x):
+    def forward(self, x, is_white):
         """
         Args:
             x: Tensor of shape (batch_size, 64) containing token IDs for each square.
@@ -102,7 +103,14 @@ class Position2MoveModel(nn.Module):
         token_emb = self.token_embedding(x)  # (batch_size, 64, embed_dim)
         pos_emb = self.position_embedding(positions)  # (batch_size, 64, embed_dim)
 
-        x = token_emb + pos_emb  # (batch_size, 64, embed_dim)
+        if is_white:
+            player_ids = torch.zeros((batch_size, seq_len), dtype=torch.long, device=x.device)
+        else:
+            player_ids = torch.ones((batch_size, seq_len), dtype=torch.long, device=x.device)
+
+        player_emb = self.player_embedding(player_ids)  # (batch_size, 64, embed_dim)
+
+        x = token_emb + pos_emb + player_emb# (batch_size, 64, embed_dim)
         x = self.dropout(x)
 
         x = self.transformer_blocks(x)  # (batch_size, 64, embed_dim)
